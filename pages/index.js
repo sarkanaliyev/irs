8d17f52d10b9ffd7dbe31aa23e3b79071a1d6de4
Sarkan Aliyev <sarkanaliyev@gmail.com>
2021-04-15 18:09:05 +0400

    dockerfile created

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ import districts_styles from "../pages/districts/Districts.module.css";
 
 import Menu from "../components/Menu";
 import Map from "../components/Map";
+import { BsArrowLeft } from "react-icons/bs";
+
+import React, { useState, useEffect } from "react";
+
 export default function Home({ districts }) {
   // const tl1 = new Timeline({ defaults: { ease: "power1.out" } });
   // useEffect(() => {
@@ -18,6 +22,19 @@ export default function Home({ districts }) {
   //     tl1.to(".slider", { y: "-100%", duration: 1.5 });
   //   }, 20000);
   // });
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    let paths = document.querySelectorAll("svg > path");
+    console.log(paths);
+
+    paths.forEach((element) => {
+      element.addEventListener("click", () => {
+        setIsOpen(true);
+      });
+    });
+  }, []);
+
   function openNav() {
     document.getElementById("myNav").style.height = "100%";
     console.log("opennav");
@@ -28,6 +45,11 @@ export default function Home({ districts }) {
   }
   return (
     <div className={styles.container}>
+      <div
+        className={`${styles.right_slider} ${
+          isOpen && styles.right_slider_active
+        }`}
+      ></div>
       <div className={monuments_styles.top}>
         <a href="/">
           <img src="/logo.svg" className={districts_styles.districts_logo} />
@@ -46,7 +68,7 @@ export default function Home({ districts }) {
       </div>
 
       {/* <img className={styles.xerite} src="/xerite-transparant.png" /> */}
-      <Map />
+      <Map className={isOpen && styles.map_minimize} />
 
       <Menu />
       {/* <div className={styles.slider}>
