d28a7572e4a237cf8661c5894a9c388543e07b21
Sarkan Aliyev <sarkanaliyev@gmail.com>
2021-04-22 18:52:17 +0400

    again again

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://abide.inetlab.info/";
+  process.env.NEXT_PUBLIC_API_URL || "https://abide.inetlab.info";
   // process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 // export const API_URL =
 //   process.env.NEXT_PUBLIC_API_URL ||
