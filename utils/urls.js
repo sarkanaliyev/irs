7f5cefe9d2f42bc6896958e80ef140a4b4a7a94c
Sarkan Aliyev <sarkanaliyev@gmail.com>
2021-06-23 19:11:26 +0400

    third commit

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "10.2.214.2";
-  // process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://10.2.214.2";
+// process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 // export const API_URL =
 //   process.env.NEXT_PUBLIC_API_URL ||
 //   "http://obscure-stream-64285.herokuapp.com/";
